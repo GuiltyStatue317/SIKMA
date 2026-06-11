8c82b45d1477cd448d391b17ec8544edc2b8613b
GuiltyStatue317 <89077697+GuiltyStatue317@users.noreply.github.com>
2026-06-11 19:43:05 +0700

    Update Supabase client initialization with real URL and key

--- docs/js/supabase.js
+++ docs/js/supabase.js
@@ -2,6 +2,6 @@ import { createClient }
 from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
 export const supabase = createClient(
-    'URL_PROJECT',
-    'ANON_KEY'
+    'https://qiuiiumhgyfsiddiynov.supabase.co',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFpdWlpdW1oZ3lmc2lkZGl5bm92Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA4MjM2NjMsImV4cCI6MjA5NjM5OTY2M30.Z6IiuSrgc2xbvbeqC5YgYRWhqopqKd7pMPliDptozik'
 )
